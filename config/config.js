e45ff1762163e5a6e405cdde4fc8950cb32b9449
mirayzd <aqilahyazid08@gmail.com>
2022-07-22 05:36:05 +0800

    addd

--- config/config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-    envPort: process.env.PORT,
-    dbURL: process.env.MONGODB_URL,
-    sessionKey: process.env.SESSION_SECRET,
-};
--- models/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = Object.assign({
-    accounts: require('./accounts'),
-    products: require('./products'),
-    orders: require('./orders'),
-    order_items: require('./order_items')
-})
