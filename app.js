eafc24f8dc54f678e5a319c05f0878f98bbb4c4b
mirayzd <aqilahyazid08@gmail.com>
2022-07-22 05:20:20 +0800

    add

--- app.js
+++ app.js
@@ -1,32 +1,113 @@
-var http = require('http');
-var express = require("express");
-var path = require ("path");
-var routes = require("./routes/home");
-var app = express();
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const hbs = require('handlebars');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const moment = require('moment');
+const { envPort, dbURL, sessionKey } = require('./config/config');
+const MongoStore = require('connect-mongo')(session);
 
-//const express = require("express");
-//const app = express();
-//const mongoose = require("mongoose");
+const app = express();
+require('./config/passport')(passport);
 
-//app.listen(3000, () => {
-//    console.log("Backend server is running!");
-//})
-
-app.set("port", process.env.PORT || 3000);
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine","ejs");
-
-app.use(routes);
-app.use('/css', express.static(path.resolve(__dirname, "views/css")))
-app.use('/img', express.static(path.resolve(__dirname, "views/img")))
-app.use('/image', express.static(path.resolve(__dirname, "views/image")))
-app.use('/js', express.static(path.resolve(__dirname, "views/js")))
+// Statics
+app.use(express.static('assets'));
+app.use(express.static(__dirname + '/public'));
+app.enable('strict routing');
+app.all('/admin', function(req, res) { res.redirect('/admin/'); });
+app.use('/admin/',express.static(__dirname+'/public'));
+app.use('/admin/',express.static('assets'));
 
 
+// MongoDB
+// const URL = "mongodb+srv://LouisD69:baloney1@pharmago-5nuy4.gcp.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
 
-app.listen(app.get("port"), function(){
-    console.log("Server started on port " + app.get("port"));
+
+
+// Handlebars
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    //defaultview: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/',
+    handlebars: allowInsecurePrototypeAccess(hbs),
+    helpers: 
+    {
+        formatDate: function(datetime) 
+        {
+            if (datetime !== null) {
+                format = "MMMM DD YYYY";
+                return moment(datetime).format(format);
+            }
+            else {
+                return "";
+            }
+        },
+
+        formatPrice: function(num)
+        {
+            return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+        }
+    }
+}))
+
+app.set('view engine', 'hbs');
+
+// Body Parser
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Flash
+app.use(flash());
+app.use( (req, res, next) => {
+    res.locals.msg = req.flash("msg");
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use((req, res, next) => {
+    res.locals.loggedIn = req.isAuthenticated();
+    next();
 });
 
+// Routes
+app.use('/', require('./routes/login'));
+app.use('/', require('./routes/register'));
+app.use('/', require('./routes/catalogue'));
+app.use('/', require('./routes/profile'));
+app.use('/', require('./routes/checkout'));
+app.use('/', require('./routes/myorders'));
+app.use('/admin', require('./routes/manageOrder'));
+app.use('/admin', require('./routes/catalogue'));
+app.use('/admin', require('./routes/manageCatalogue'));
+app.use('/admin', require('./routes/adminProf'));
 
+// Port
+const port = envPort || 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
